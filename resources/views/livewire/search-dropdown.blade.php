810aa8772d2afc7853ea5c4941f9ab204f62e8c8
FrsErley <frsleeh@gmail.com>
2023-03-04 00:38:35 -0300

    Space filmes: Search bar finished

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,27 +1,37 @@
-<div>
+<div x-data="{ isOpen: true}" @click.away="isOpen = false">
 
-    <div style="position:relative;" class="mr-4">
-        <form style="display: flex" class="form-inline mt-2 mt-md-0" action="/" method="GET">
-            <input wire:model.debounce.500ms="search" style="border-radius: 25px" class="form-control" type="text" placeholder="Pesquise seu filme.." id="search" name="search" aria-label="Search">
+    <div>
+        <form class="form-inline " action="/" method="GET">
+            <input 
+                wire:model.debounce.500ms="search" style="border-radius: 25px" class="form-control" type="text" placeholder="Pesquise seu filme.." id="search" name="search" aria-label="Search"
+                @focus=" isOpen = true "
+                @keydown=" isOpen = true "
+                @keydown.escape.window="isOpen = false"
+                @keydown.shift.tab="isOpen = false"
+            >
         </form>
     </div>
 
     @if (strlen($search) >= 2)
-    <div id="search-bar" style= "border-radius:8px; display:block">
+    
 
         @if ($searchResults->count() > 0)
+
+        <div id="search-bar" style= "border-radius:8px; position:absolute"
+        x-show="isOpen">
 
                 <ul class="lista-filmes" style="list-style-type:none; border-radius: 8px">
                     @foreach ($searchResults as $result)
                         
-                            <a style="text-decoration:none; color: white" href="/movie/{{$result['id']}}">  
-                                <li class=" search-click border-bottom p-3">
+                            <a style="text-decoration:none; color: white;" href="/movie/{{$result['id']}}">  
+                                <li class=" search-click" style=" display:flex; width:260px; padding:12px; border-bottom: 1px solid; border-color:rgba(240, 248, 255, 0.218)">
+                                {{-- Alterar tamanho baseado no tamanho do titulo do filme --}}
                                 @if ($result['poster_path'])
-                                    <img style="width: 40px; margin-right: 8px" src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster">
+                                    <img style="width: 50px; margin-right: 8px" src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster">
                                 @else
-                                <img style="width: 40px; margin-right: 8px; display: inline" src="https://via.placeholder.com/50x70" alt="poster">
+                                <img style="width: 50px; margin-right: 8px;" src="https://via.placeholder.com/50x70" alt="poster">
                                 @endif
-                                <span> {{$result['title']}} </span>
+                                <span class="d-flex align-items-center"> {{$result['title']}} </span>
                                 </li>
                             </a>
                         
